83fa90de693279f18d7084574950a14954aeccd4
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-12-17 00:48:21 +0200

    Rendering progress bars

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-console.log(mainjs);
+import { progressBarData } from './data/progressBarData.js';
+import { renderProgressBar } from './components/progress-bar/renderProgressBar.js';
+
+renderProgressBar('.left', progressBarData);
+
