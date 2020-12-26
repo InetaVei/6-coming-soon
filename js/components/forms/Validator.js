2532bbc5017f89db901192c043e0e30ee09ef8ca
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-12-27 00:23:32 +0200

    only first uppercase letter

--- js/components/forms/Validator.js
+++ js/components/forms/Validator.js
@@ -1,7 +1,22 @@
 class Validator {
-    static isValidName( name ) {
+    static isValidName(name) {
+        // ne tuscias tekstas
+        const notEmptyString = Validator.notEmptyString(name);
+        if (notEmptyString !== true) return notEmptyString;
+
+        //tik abeceles raides
+        const onlyAlphabet = Validator.onlyAlphabet(name);
+        if (onlyAlphabet !== true) return onlyAlphabet;
+
+        // tik pirma raide didzioji
+        const onlyFirstLetterUppercase = Validator.onlyFirstLetterUppercase(name);
+        if (onlyFirstLetterUppercase !== true) return onlyFirstLetterUppercase;
+
         return true;
     }
+
+
+
     static isValidEmail( email ) {
         return true;
     }
@@ -16,6 +31,21 @@ class Validator {
         if (text === '') {
             return 'Turi buti ne tuscias tekstas';
         }
+        return true;
+    }
+    static onlyAlphabet(text) {
+        return true;
+    }
+    static onlyFirstLetterUppercase(text) {
+        // 1. visas raides konvertuoti i mazasias
+        let lower = text.toLowerCase();
+        // 2. pirmaja raide paversti didziaja
+        let convert = lower[0].toUpperCase() + lower.slice(1);
+        // 3. palyginame ka gavome su inputu 
+        if (text !== convert) {
+            return 'Pirmasis simbolis turi buti didzioji raide, o visi kiti - mazosios'
+        }
+
         return true;
     }
 }
